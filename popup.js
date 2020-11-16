ac4f4644096853d8f132a8d6835fa124c32f029c
xdynasty <xdynasty5201314@gmail.com>
2020-11-16 01:49:28 -0500

    fix canvas image display

--- popup.js
+++ popup.js
@@ -6,28 +6,21 @@ eyedropperBtn.addEventListener('click', () => {
     },
     (dataUrl) => {
       chrome.tabs.executeScript({
-        //   code: `const canvas = document.createElement('canvas');
-        // canvas.id = 'canvas';
-        // canvas.style.width = window.innerWidth;
-        // canvas.style.height = window.innerHeight;
-        // canvas.style.position = "absolute";
-        // canvas.style.top = 0;
-        // canvas.style.left = 0;
-        // const context = canvas.getContext('2d');
-        // const img = new Image();
-        // img.src = "${dataUrl}";
-        // img.onload = () => {
-        //   context.drawImage(img, 0, 0)
-        // }
-        // document.body.appendChild(canvas);
-        // `,
-        code: `
-        img = document.createElement("img");
+        code: `const canvas = document.createElement('canvas');
+        canvas.id = 'canvas';
+        canvas.style.position = "absolute";
+        canvas.style.top = 0;
+        canvas.style.left = 0;
+        const context = canvas.getContext('2d');
+        context.canvas.width = window.innerWidth;
+        context.canvas.height = window.innerHeight;
+        const img = new Image();
         img.src = "${dataUrl}";
-        img.style.width = "100vw";
-        img.style.height = "100vh";
-        document.body.appendChild(img);
-      `,
+        img.onload = () => {
+          context.drawImage(img, 0, 0, window.innerWidth, window.innerHeight)
+        }
+        document.body.appendChild(canvas);
+        `,
       });
     }
   );
