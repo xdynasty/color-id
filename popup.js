11fdb305771b1e854a39f5dc48654bd9bad925ef
xdynasty <xdynasty5201314@gmail.com>
2020-11-16 11:42:49 -0500

    add zoom canvas

--- popup.js
+++ popup.js
@@ -6,11 +6,13 @@ eyedropperBtn.addEventListener('click', () => {
     },
     (dataUrl) => {
       chrome.tabs.executeScript({
-        code: `const canvas = document.createElement('canvas');
+        code: `
+        const canvas = document.createElement('canvas');
         canvas.id = 'canvas';
         canvas.style.position = "absolute";
         canvas.style.top = 0;
         canvas.style.left = 0;
+        canvas.style.zIndex = "99999";
         const context = canvas.getContext('2d');
         context.canvas.width = window.innerWidth;
         context.canvas.height = window.innerHeight;
@@ -20,6 +22,39 @@ eyedropperBtn.addEventListener('click', () => {
           context.drawImage(img, 0, 0, window.innerWidth, window.innerHeight)
         }
         document.body.appendChild(canvas);
+        const zoomCanvas = document.createElement('canvas');
+        zoomCanvas.id = "zoomCanvas";
+        zoomCanvas.style.position = "absolute";
+        zoomCanvas.style.top = 0;
+        zoomCanvas.style.left = 0;
+        zoomCanvas.style.zIndex = "99999";
+        const zoomCtx = zoomCanvas.getContext('2d');
+        zoomCtx.canvas.width = 200;
+        zoomCtx.canvas.height = 200;
+        zoomCtx.imageSmoothingEnabled = false;
+        zoomCtx.mozImageSmoothingEnabled = false;
+        zoomCtx.webkitImageSmoothingEnabled = false;
+        zoomCtx.msImageSmoothingEnabled = false;
+        document.body.appendChild(zoomCanvas)
+
+        function zoom(ctx, x, y) {
+          console.log("drawing")
+          ctx.drawImage(canvas,
+            Math.min(Math.max(0, x - 3), img.width - 3),
+            Math.min(Math.max(0, y - 3), img.height - 3),
+            11, 11,
+            0, 0,
+            200, 200);
+        };
+
+        canvas.addEventListener('mousemove', function(event) {
+          const x = event.layerX;
+          const y = event.layerY;
+          console.log("x", x);
+          console.log("y", y);
+          zoom(zoomCtx, x, y);
+        });
+
         `,
       });
     }
